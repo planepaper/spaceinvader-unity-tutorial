f81a937dcca65ed1b7a44f27993190896230caac
planepaper <eddysonkr@gmail.com>
2022-04-28 22:37:24 +0900

    FEAT: Make Invaders Move Side To Side
    - Render Prefabs by Invaders Script.

--- Assets/Scripts/Invaders.cs
+++ Assets/Scripts/Invaders.cs
@@ -5,6 +5,9 @@ public class Invaders : MonoBehaviour
     public Invader[] prefabs;
     public int rows = 5;
     public int columns = 11;
+    public float speed = 1.0f;
+
+    private Vector3 _direction = Vector2.right;
 
     private void Awake()
     {
@@ -24,5 +27,41 @@ public class Invaders : MonoBehaviour
                 invader.transform.localPosition = position;
             }
         }
+    }
+
+    private void Update()
+    {
+        transform.position += _direction * speed * Time.deltaTime;
+
+        Vector3 leftEdge = Camera.main.ViewportToWorldPoint(Vector3.zero);
+        Vector3 rightEdge = Camera.main.ViewportToWorldPoint(Vector3.right);
+
+        foreach (Transform invader in transform)
+        {
+            if (!invader.gameObject.activeInHierarchy)
+            {
+                continue;
+            }
+
+            if (_direction == Vector3.right && invader.position.x >= (rightEdge.x - 1f))
+            {
+                AdvanceRow();
+                break;
+            }
+            else if (_direction == Vector3.left && invader.position.x <= (leftEdge.x + 1f))
+            {
+                AdvanceRow();
+                break;
+            }
+        }
+    }
+
+    private void AdvanceRow()
+    {
+        _direction = new Vector3(-_direction.x, 0f, 0f);
+
+        Vector3 position = transform.position;
+        position.y -= 1f;
+        transform.position = position;
     }
 }
