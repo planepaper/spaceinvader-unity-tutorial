5d555d45d7fca2b5ee07072b2e3ccb4aad976114
planepaper <eddysonkr@gmail.com>
2022-05-01 23:39:00 +0900

    FEAT: Make Player Spaceship Move
    - Implement Player Script.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 5.0f;
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position += Vector3.left * speed * Time.deltaTime;
+        }
+        else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position += Vector3.right * speed * Time.deltaTime;
+        }
+    }
+}
